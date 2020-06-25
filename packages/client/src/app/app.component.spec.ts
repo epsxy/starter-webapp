946f8ec2e0c1004685bd6b76ba2e2244a4b68c27
epsxy <epsxy@users.noreply.github.com>
2020-06-26 00:55:37 +0200

    test(client): fix client basic test

--- packages/client/src/app/app.component.spec.ts
+++ packages/client/src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
         const fixture = TestBed.createComponent(AppComponent);
         fixture.detectChanges();
         const compiled = fixture.debugElement.nativeElement;
-        expect(compiled.querySelector('.content span').textContent).toContain('client app is running!');
+        expect(compiled.querySelector('.root__title').textContent).toContain('Hellow');
     });
 });
