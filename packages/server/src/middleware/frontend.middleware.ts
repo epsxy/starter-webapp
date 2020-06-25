d424f593035a3501126d54c960f9d890943a8ce1
epsxy <epsxy@users.noreply.github.com>
2020-06-26 00:45:50 +0200

    refactor: adapt coding style to prettier

--- packages/server/src/middleware/frontend.middleware.ts
+++ packages/server/src/middleware/frontend.middleware.ts
@@ -2,18 +2,7 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { join, resolve } from 'path';
 
-const allowedExt = [
-    '.js',
-    '.map',
-    '.css',
-    '.ico',
-    '.png',
-    '.jpg',
-    '.svg',
-    '.woff2',
-    '.woff',
-    '.ttf',
-];
+const allowedExt = ['.js', '.map', '.css', '.ico', '.png', '.jpg', '.svg', '.woff2', '.woff', '.ttf'];
 
 /**
  * Behavior is the following:
@@ -27,20 +16,13 @@ export class FrontendMiddleware implements NestMiddleware {
         const url = req.baseUrl;
         if (/^\/api\/(\S+)?$/.test(url)) {
             next();
-        } else if (
-            allowedExt.map(ext => url.endsWith(ext)).filter(entry => entry)
-                .length > 0
-        ) {
+        } else if (allowedExt.map(ext => url.endsWith(ext)).filter(entry => entry).length > 0) {
             /**
              * Got an extension, resolve file
              */
-            res.sendFile(
-                resolve(join('..', 'client', 'dist', 'client', `${url}`)),
-            );
+            res.sendFile(resolve(join('..', 'client', 'dist', 'client', `${url}`)));
         } else {
-            res.sendFile(
-                resolve(join('..', 'client', 'dist', 'client', 'index.html')),
-            );
+            res.sendFile(resolve(join('..', 'client', 'dist', 'client', 'index.html')));
         }
     }
 }
