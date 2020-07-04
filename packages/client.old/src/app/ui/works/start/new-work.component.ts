b7dd6d0d98b3eb391e63fa0f375c071b1e8cd45d
epsxy <epsxy@users.noreply.github.com>
2020-07-04 02:56:15 +0200

    chore: remove client.old files

--- packages/client.old/src/app/ui/works/start/new-work.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-new-work',
-    templateUrl: 'new-work.component.html',
-    styleUrls: ['new-work.component.sass'],
-})
-export class NewWorkComponent {}
